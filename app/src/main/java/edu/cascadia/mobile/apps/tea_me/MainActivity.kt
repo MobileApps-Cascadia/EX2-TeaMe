554e420c0a71af117a0296d7cbb0af3239a5c2d3
Sandeep <fredericdouamba2014@gmail.com>
2022-10-17 18:45:58 -0700

    -m "Exercise 2: Tea Me"

--- app/src/main/java/edu/cascadia/mobile/apps/tea_me/MainActivity.kt
+++ app/src/main/java/edu/cascadia/mobile/apps/tea_me/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.cascadia.mobile.apps.tea_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.CheckBox
 import android.widget.CompoundButton
 import android.widget.Toast
@@ -21,23 +22,32 @@ class MainActivity : AppCompatActivity() {
         sugarCheckBox = findViewById(R.id.sugarBox)
         lemonCheckBox = findViewById(R.id.lemonBox)
 
-        //TODO: Assign each checkbox an EventHandler using a setOnCheckedChangeListener to calls the clickIt function with two parameters
-
-    }
-
-    fun clickIt(box: CompoundButton, selected: Boolean ) {
-        //Add or remove the items text value to ArrayList depending on whether it was checked or unchecked
-        if (selected) TeaGoodies.add(box.text)
-        else TeaGoodies.remove(box.text)
-
-        //Create a String of the current set of labels in the TeaGoodies Array
-        //TODO: Add the word "Love" to the end of every string built
-        val goodiesString = StringBuilder("Tea with ")
-        for (goodies in TeaGoodies) {
-            goodiesString.append(goodies).append(", ")
+        //: Assign each checkbox an EventHandler using a setOnCheckedChangeListener to calls the clickIt function with two parameters
+        milkCheckBox.setOnClickListener { view: View ->
+            Toast.makeText(this, "milk check box", Toast.LENGTH_SHORT).show()
         }
-        //Display the string as a temporary popup - called a "Toast"
-        Toast.makeText(this@MainActivity, goodiesString, Toast.LENGTH_SHORT).show()
-    }
+        sugarCheckBox.setOnClickListener { view: View ->
+            Toast.makeText(this, "sugar check box", Toast.LENGTH_SHORT).show()
+        }
 
+        lemonCheckBox.setOnClickListener { view: View ->
+            Toast.makeText(this, "lemon check box", Toast.LENGTH_SHORT).show()
+        }
+
+        fun clickIt(box: CompoundButton, selected: Boolean) {
+            //Add or remove the items text value to ArrayList depending on whether it was checked or unchecked
+            if (selected) TeaGoodies.add(box.text)
+            else TeaGoodies.remove(box.text)
+
+            //Create a String of the current set of labels in the TeaGoodies Array
+            //TODO: Add the word "Love" to the end of every string built
+            val goodiesString = StringBuilder("Tea with ")
+            for (goodies in TeaGoodies) {
+                goodiesString.append(goodies).append(",Love ").append(clickIt(box, selected))
+            }
+            //Display the string as a temporary popup - called a "Toast"
+            Toast.makeText(this@MainActivity, goodiesString, Toast.LENGTH_SHORT).show()
+        }
+
+    }
 }
